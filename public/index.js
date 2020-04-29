a420e28cf34d9c349d813ea63bddd728ca284850
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2020-04-29 17:46:05 -0400

    added greeting

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
 setTimeout(function(){
-$("#div1").css({opacity:0, visibility: "visible"}).animate({opacity:1}, "slow")
+// $("#div1").css({opacity:0, visibility: "visible"}).animate({opacity:1}, "slow")
+$("#div1").css({opacity:0}).animate({opacity:1}, "slow")
 },500);
 
 
