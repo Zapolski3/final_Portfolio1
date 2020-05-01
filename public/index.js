05a0b92f3b3f7894d74341f6b510c87936494d03
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2020-05-01 01:23:37 -0400

    added

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
-setTimeout(function(){
-// $("#div1").css({opacity:0, visibility: "visible"}).animate({opacity:1}, "slow")
-$("#div1").css({opacity:0}).animate({opacity:1}, "slow")
-},500);
+setTimeout(function(){ $("#div1").css({opacity:0}).animate({opacity:1}, "slow")},500);
+// setTimeout(function(){ $("#div1").css({height:0}).animate({height:"h1"}, "slow")},500);
+
+setTimeout(function(){ $("#parent").css({height:0, visibility:"visible"}).animate({height:"450px"}, "slow")},1000);
+
+
 
 
 
