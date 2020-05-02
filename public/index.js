41ca5e5b13c34494238386dd6d9b497b4895a11b
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2020-05-02 02:00:28 -0400

    added text

--- public/index.js
+++ public/index.js
@@ -1,26 +1,21 @@
 $(document).ready(function(){
     
 setTimeout(function(){ $("#div1").css({opacity:0}).animate({opacity:1}, "slow")},500);
-// setTimeout(function(){ $("#div1").css({height:0}).animate({height:"h1"}, "slow")},500);
 
-// setTimeout(function(){ $("#parent").css({height:0, visibility:"visible"}).animate({height:"450px"}, "slow")},1000);
 
-$("#button1").click("click",function(){
-    $("#aboutMe").attr("class","visible");
-    $("#techUsed").attr("class","hidden");
-    $("#img").attr("class","visible");
-})
+$("#button1").on("click", function(){
 
-$("#button2").click("click",function(){
-    $("#aboutMe").attr("class","hidden");
-    $("#techUsed").attr("class","visible");
-    $("#img").attr("class","hidden");
+    $("#aboutMe").removeClass("hide");
+    $("#techUsed").attr("class","hide");
 })
 
 
 
-
-
+$("#button2").on("click", function(){
+    
+    $("#aboutMe").attr("class","hide");
+    $("#techUsed").removeClass("hide");
+})
 
 
 });
