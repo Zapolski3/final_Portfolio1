ce3f155e1005c0173a91668f5abc0a4ad56a13f6
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2020-05-04 01:27:01 -0400

    added animated effects

--- public/index.js
+++ public/index.js
@@ -3,11 +3,30 @@ $(document).ready(function(){
 setTimeout(function(){ $("#div1").css({opacity:0}).animate({opacity:1}, "slow")},500);
 
 
-$("#button1").on("click", function(){
+// function result(){
+//     // var t will store height of div with "parent id"
+//     var t = $("#parent").height();
+//     console.log(t);
 
+
+// }
+
+
+
+
+
+$("#button1").on("click", function(){
+    
     $("#aboutMe").removeClass("hide");
     $("#techUsed").attr("class","hide");
-})
+   
+    var result = $("#img").height();
+    console.log(result)
+
+    $("#parent").animate({height:"577px"}, "slow");
+
+    console.log(result)
+});
 
 
 
@@ -15,7 +34,13 @@ $("#button2").on("click", function(){
     
     $("#aboutMe").attr("class","hide");
     $("#techUsed").removeClass("hide");
-})
+   
+    var result = $("#techText").height() + 15;
+    console.log(result)
+
+    $("#parent").animate({height: result}, "slow");
+
+});
 
 
 });
